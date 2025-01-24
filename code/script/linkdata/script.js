16511c874c4858c46ae221c6268d4170cd579e18
DevRafaSilva <developerrafasilva@gmail.com>
2025-01-24 20:23:06 -0300

    Rota protegida

--- code/script/linkdata/script.js
+++ code/script/linkdata/script.js
@@ -1,4 +1,7 @@
 import linkMudar from './linkMudar.js';
+import rotaProtegida from './rotaProtegida.js';
 
 const link = new linkMudar('[data-link]');
 link.init();
+const rota = new rotaProtegida();
+rota.init();
